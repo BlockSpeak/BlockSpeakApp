8808f78e068a5785af679ee9821c64d7896663dc
BlockSpeakApp <blockspeaks@gmail.com>
2025-03-09 16:44:59 -0700

    hardhatconfig adjusted for deployment switch

--- server/skillchain_contracts/hardhat.config.js
+++ server/skillchain_contracts/hardhat.config.js
@@ -1,15 +1,21 @@
+require('dotenv').config();
 require("@nomicfoundation/hardhat-toolbox");
 require("@nomicfoundation/hardhat-ignition");
 
 module.exports = {
-    solidity: "0.8.28", // Matches SkillNFT.sol, works with ^0.8.0
+    solidity: "0.8.28", // Matches SkillNFT.sol
     networks: {
         hardhat: {
             chainId: 31337
+        },
+        sepolia: {
+            url: `https://eth-sepolia.g.alchemy.com/v2/${process.env.ALCHEMY_API_KEY}`,
+            accounts: [process.env.PRIVATE_KEY],
+            chainId: 11155111
         }
     },
     paths: {
-        sources: "./contracts",    // Look here for .sol files
-        artifacts: "./artifacts"   // Output here
+        sources: "./contracts",
+        artifacts: "./artifacts"
     }
 };
